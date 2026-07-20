1f07b7906bb455dba3e21150a5212ecf9f4045b6
Sandip Panda <sandippanda577@gmail.com>
2026-07-20 21:22:17 +0530

    Lombok Add

--- src/main/java/com/ecommerce/Ecom_Model/CategoryModel.java
+++ src/main/java/com/ecommerce/Ecom_Model/CategoryModel.java
@@ -5,39 +5,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CategoryModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
-    // No-arg constructor
-    public CategoryModel() {
-    }
-
-    // Constructor
-    public CategoryModel(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    // Getters and Setters
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
